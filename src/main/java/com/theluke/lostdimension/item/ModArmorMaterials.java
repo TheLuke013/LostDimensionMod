de26dd6e750f4baa3dd0714201f08dc15043d8f1
TheLuke013 <araujochagaslucas2@gmail.com>
2024-07-06 16:21:20 -0300

    Added Lost Armor(with bug)

---
+++ src/main/java/com/theluke/lostdimension/item/ModArmorMaterials.java
@@ -0,0 +1,78 @@
+package com.theluke.lostdimension.item;
+
+import com.theluke.lostdimension.LostDimension;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+    LOST("lost", 25, new int[]{3, 8, 6, 3}, 19,
+            SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 2f, 0.1f, () -> Ingredient.ofItems(ModItems.LOST_INGOT));
+
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] protectionAmounts;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Supplier<Ingredient> repairIngredient;
+
+    private static final int[] BASE_DURABILITY = {13, 15, 16, 11};
+
+    ModArmorMaterials(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantability, SoundEvent equipSound,
+                      float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantability = enchantability;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()] * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return protectionAmounts[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.equipSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @Override
+    public String getName() {
+        return LostDimension.MOD_ID + ":" + this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
--- src/main/java/com/theluke/lostdimension/item/ModItems.java
+++ src/main/java/com/theluke/lostdimension/item/ModItems.java
@@ -1,24 +1,35 @@
 package com.theluke.lostdimension.item;
 
 import com.theluke.lostdimension.LostDimension;
-
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.*;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-	public static final Item LOST_INGOT = registerItem("lost_ingot",
-	new Item(new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
-	
-	public static final Item LOST_SWORD = registerItem("lost_sword",
-	new SwordItem(ModToolMaterial.LOST, 5, 3f, new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
+    public static final Item LOST_INGOT = registerItem("lost_ingot",
+            new Item(new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
 
-	private static Item registerItem(String name, Item item) {
-		return Registry.register(Registry.ITEM, new Identifier(LostDimension.MOD_ID, name), item);
-	}
+    public static final Item LOST_SWORD = registerItem("lost_sword",
+            new SwordItem(ModToolMaterial.LOST, 10, 3f, new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
 
-	public static void registerModItems() {
+    public static final Item LOST_HELMET = registerItem("lost_helmet",
+            new ArmorItem(ModArmorMaterials.LOST, EquipmentSlot.HEAD, new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
+    public static final Item LOST_CHESTPLATE = registerItem("lost_chestplate",
+            new ArmorItem(ModArmorMaterials.LOST, EquipmentSlot.CHEST, new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
+    public static final Item LOST_LEGGINGS = registerItem("lost_leggings",
+            new ArmorItem(ModArmorMaterials.LOST, EquipmentSlot.LEGS, new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
+    public static final Item LOST_BOOTS = registerItem("lost_boots",
+            new ArmorItem(ModArmorMaterials.LOST, EquipmentSlot.FEET, new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(LostDimension.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
         LostDimension.LOGGER.debug("Registrando itens do mod " + LostDimension.MOD_ID);
-	}
+    }
 }
