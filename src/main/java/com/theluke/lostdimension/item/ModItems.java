74e76979b5f2bbfca7d94511c68c6e755128f56b
TheLuke013 <araujochagaslucas2@gmail.com>
2024-07-06 15:17:22 -0300

    Added Lost Sword

--- src/main/java/com/theluke/lostdimension/item/ModItems.java
+++ src/main/java/com/theluke/lostdimension/item/ModItems.java
@@ -3,13 +3,16 @@ package com.theluke.lostdimension.item;
 import com.theluke.lostdimension.LostDimension;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 	public static final Item LOST_INGOT = registerItem("lost_ingot",
 	new Item(new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
+	
+	public static final Item LOST_SWORD = registerItem("lost_sword",
+	new SwordItem(ModToolMaterial.LOST, 5, 3f, new FabricItemSettings().group(ModItemGroup.LOST_GROUP)));
 
 	private static Item registerItem(String name, Item item) {
 		return Registry.register(Registry.ITEM, new Identifier(LostDimension.MOD_ID, name), item);
